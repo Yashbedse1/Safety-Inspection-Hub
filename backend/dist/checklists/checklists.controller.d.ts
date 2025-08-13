6c1314099666b3796e2780df470d147bcc5a32a1
YashBedse1 <bedseyash56@gmail.com>
2025-08-13 15:03:06 +0100

    chore: remove build artefacts and dependencies from repo

--- backend/dist/checklists/checklists.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { ChecklistsService } from './checklists.service';
-import { ChecklistSummary } from './types/checklist.types';
-export declare class ChecklistsController {
-    private readonly checklistsService;
-    constructor(checklistsService: ChecklistsService);
-    getAllSummaries(): ChecklistSummary[];
-    getById(id: string): import("./types/checklist.types").Checklist;
-}
